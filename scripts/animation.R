94b486cbb20ea46a1c0d776dc63db2f941b80119
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2025-08-08 22:04:16 +0200

    update to site

--- scripts/animation.R
+++ scripts/animation.R
@@ -4,10 +4,25 @@ library(tidyverse)
 library(gganimate)
 load("data/cleaned/data.RData")
 
-p <- 
-  TOF_long %>% 
-  ggplot(aes(x=x.pos, y=y.pos, fill = dist1)) + 
-  labs(title = "TOF at {frame_time}") +
-  extras + transition_time(Datetime)
+#set visualization parameters
+extras <- list(
+  geom_tile(),
+  scale_fill_viridis_c(direction = -1, limits = c(0, 200),
+                       oob = scales::oob_squish_any),
+  theme_minimal(),
+  guides(colour = "none"),
+  coord_fixed(),
+  labs(x = "X position (°)", y = "Y position (°)", 
+       fill = "Distance (cm)", alpha = "Confidence (0-255)"))
 
-animate(p, duration = 60, fps = 5)
+p <-
+  dataVEET3 |> filter_Datetime(start = "2024-06-10 13:10:00", length = "5 mins") |> 
+    add_Time_col() |> 
+  mutate(dist1 = ifelse(dist1 == 0, Inf, dist1)) |> #replace 0 distances with 5m
+  filter(conf1 >= 0.1 | dist1 == Inf) |> #remove data that has less than 10% confidence
+  ggplot(aes(x=x.pos, y=y.pos, fill = dist1/10))+ #plot the data
+  labs(title = "Spatially resolved distance from eye-level at {closes_state}") +
+  extras + transition_time(Time)
+
+anim <- animate(p, duration = 60, fps = 24)
+anim
